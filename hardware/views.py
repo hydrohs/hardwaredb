f39bb72add2e506d98e56cbe71b97357480ceffa
Hydrohs <03je542u@duck.com>
2022-11-08 09:47:59 -0400

    Update homepage

--- hardware/views.py
+++ hardware/views.py
@@ -3,8 +3,35 @@ from django.views.generic import ListView
 
 from .models import CPU, RAM, GPU, Cables, SoundCard, ExpansionCard, NIC, Motherboard, Case, Peripheral, PSU
 
-def index(request):
-    return render(request, 'hardware/index.html')
+def index(requet):
+    # Count number of objects for all classes
+    num_cpu = CPU.objects.count()
+    num_ram = RAM.objects.count()
+    num_gpu = GPU.objects.count()
+    num_cables = Cables.objects.count()
+    num_sound = SoundCard.objects.count()
+    num_exp = ExpansionCard.objects.count()
+    num_nic = NIC.objects.count()
+    num_mb = Motherboard.objects.count()
+    num_case = Case.objects.count()
+    num_periph = Peripheral.objects.count()
+    num_psu = PSU.objects.count()
+
+    context = {
+        'num_cpu': num_cpu,
+        'num_ram': num_ram,
+        'num_gpu': num_gpu,
+        'num_cables': num_cables,
+        'num_sound': num_sound,
+        'num_exp': num_exp,
+        'num_nic': num_nic,
+        'num_mb': num_mb,
+        'num_case': num_case,
+        'num_periph': num_periph,
+        'num_psu': num_psu,
+    }
+
+    return render(requet, 'hardware/index.html', context=context)
 
 class CPUList(ListView):
     model = CPU
